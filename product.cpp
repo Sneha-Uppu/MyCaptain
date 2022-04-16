dd379a99930a57f297c5efd34ed173a5b4cc51ca
Sneha-Uppu <81636785+Sneha-Uppu@users.noreply.github.com>
2022-04-16 15:09:19 +0530

    Add files via upload
    Product of 2 numbers

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double a,b,pro;
+	cout<<"Enter first number ";
+	cin>>a;
+	cout<<"Enter second number ";
+	cin>>b;
+	pro=a*b;
+	cout<<"Product of "<<a<<" and "<<b<<" is "<<pro;
+	return 0;
+}
