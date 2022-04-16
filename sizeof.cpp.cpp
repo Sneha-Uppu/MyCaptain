1ee919630e64f963cd902632176bc66c1079e4d9
Sneha-Uppu <81636785+Sneha-Uppu@users.noreply.github.com>
2022-04-16 15:03:31 +0530

    Add files via upload

---
+++ sizeof.cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	int a;
+	float b;
+	double c;
+	cout<<"Size of char is " << sizeof(ch)<<"\n";
+	cout<<"Size of int is " <<sizeof(a)<<"\n";
+	cout<<"Size of float is " <<sizeof(b)<<"\n";
+	cout<<"Size of double is " <<sizeof(c);
+}
